d3e5e878a8fc49f310e661976de4d9adf2e93110
Nishantyadav2552 <preeti9416938@gmail.com>
2024-05-21 14:02:22 +0530

    .

--- conditional.java
+++ conditional.java
@@ -1,5 +1,20 @@
+
+import java.util.Scanner;
+
 public class conditional {
     public static void main(String[] args) {
-        
+        Scanner sc = new  Scanner(System.in);
+        int a = sc.nextInt();
+        switch (a) {
+            case 1:
+                System.out.println("monday");
+                break;
+            case 2:
+                System.out.println("tuesday");
+                break;
+            default:
+                System.out.println("hello");
+                break;
+        }
     }
 }
