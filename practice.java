2d88d8081dcdb9a002cf5c3b8ddbe3ff8d566013
Nishantyadav2552 <preeti9416938@gmail.com>
2024-05-21 13:06:42 +0530

    .

---
+++ practice.java
@@ -0,0 +1,8 @@
+public class practice {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.print("hello ji \n");
+        System.out.print("ok ji");
+        
+    }
+}
